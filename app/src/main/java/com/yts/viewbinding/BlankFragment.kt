546fe746cadadc089d6db1d699e8e9a1582736a0
yts <sky877kr@gmail.com>
2020-02-28 10:45:05 +0900

    Initial commit

---
+++ app/src/main/java/com/yts/viewbinding/BlankFragment.kt
@@ -0,0 +1,62 @@
+package com.yts.viewbinding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yts.viewbinding.databinding.ActivityMainBinding
+import com.yts.viewbinding.databinding.FragmentBlankBinding
+
+class BlankFragment : Fragment() {
+    private lateinit var binding: FragmentBlankBinding
+
+    private val ARG_PARAM1 = "param1"
+    private val ARG_PARAM2 = "param2"
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBlankBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BlankFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
